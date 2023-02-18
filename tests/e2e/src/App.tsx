7438f9ce4c87ae5c584fae180cdccbf4d3b7a940
ot07 <ot07neoki@gmail.com>
2023-02-19 03:14:05 +0900

    add e2e test app

---
+++ tests/e2e/src/App.tsx
@@ -0,0 +1,71 @@
+import { Button } from './Button'
+import {
+  ISoundFont2SynthNode,
+  createSoundFont2SynthNode,
+} from '../../../dist/sf2-synth-audio-worklet'
+import { FC, useCallback, useState } from 'react'
+
+const sf2URL = new URL('./assets/GeneralUser GS v1.471.sf2', import.meta.url)
+
+const keys: { color: 'black' | 'white'; label: string; key: number }[] = [
+  { color: 'white', label: 'C', key: 60 },
+  { color: 'black', label: 'C#', key: 61 },
+  { color: 'white', label: 'D', key: 62 },
+  { color: 'black', label: 'D#', key: 63 },
+  { color: 'white', label: 'E', key: 64 },
+  { color: 'white', label: 'F', key: 65 },
+  { color: 'black', label: 'F#', key: 66 },
+  { color: 'white', label: 'G', key: 67 },
+  { color: 'black', label: 'G#', key: 68 },
+  { color: 'white', label: 'A', key: 69 },
+  { color: 'black', label: 'A#', key: 70 },
+  { color: 'white', label: 'B', key: 71 },
+  { color: 'white', label: 'C', key: 72 },
+]
+
+export const App: FC = () => {
+  const [node, setNode] = useState<ISoundFont2SynthNode | undefined>(undefined)
+
+  const start = useCallback(async () => {
+    const audioContext = new AudioContext()
+    const node = await createSoundFont2SynthNode(audioContext, sf2URL)
+    node.connect(audioContext.destination)
+    setNode(node)
+  }, [])
+
+  const playNote = useCallback(
+    (key: number) => {
+      node?.noteOn(0, key, 100, 0)
+    },
+    [node]
+  )
+
+  const stopNote = useCallback(
+    (key: number) => {
+      node?.noteOff(0, key, 0)
+    },
+    [node]
+  )
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center text-gray-800 space-y-12">
+      <Button width={816} disabled={!!node} onClick={start}>
+        Start
+      </Button>
+      <div className="flex space-x-4">
+        {keys.map(({ color, label, key }) => (
+          <Button
+            key={key}
+            width={48}
+            color={color}
+            disabled={!node}
+            onMouseDown={() => playNote(key)}
+            onMouseUp={() => stopNote(key)}
+          >
+            {label}
+          </Button>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ tests/e2e/src/Button.tsx
@@ -0,0 +1,28 @@
+import { ComponentPropsWithoutRef, FC } from 'react'
+
+type Props =
+  | {
+      width: number
+      color?: 'black' | 'white'
+    } & ComponentPropsWithoutRef<'button'>
+
+export const Button: FC<Props> = ({
+  width,
+  color = 'black',
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      style={{ width: `${width}px` }}
+      className={`focus:outline-none active:ring-2 active:ring-slate-400 active:ring-offset-2 active:ring-offset-slate-50 font-semibold h-12 rounded-lg flex items-center justify-center ${
+        color === 'black'
+          ? 'bg-slate-900 enabled:hover:bg-slate-700 text-white disabled:opacity-25'
+          : 'bg-white enabled:hover:bg-slate-100 border-2 border-slate-900 enabled:hover:border-slate-700 text-slate-900 disabled:opacity-25'
+      }`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
