001fdb043e62bae43cc67ef29450e01339a8aa1f
ot07 <ot07neoki@gmail.com>
2023-03-28 23:03:44 +0900

    remove baseUrl and paths of tsconfig.json

--- src/processor.ts
+++ src/processor.ts
@@ -1,14 +1,13 @@
 import { PROCESSOR_NAME } from './constants.js'
+import init, {
+  WasmSoundFontSynth,
+} from './generated/wasm/sf2_synth_audio_worklet_wasm'
 import './text-encoder-decoder.js'
-
 import {
   type PresetHeader,
   type SoundFont2SynthNodeMessageData,
   type SoundFont2SynthProcessorMessageData,
-} from '@/types'
-import init, {
-  WasmSoundFontSynth,
-} from './generated/wasm/sf2_synth_audio_worklet_wasm'
+} from './types'
 
 interface SoundFont2SynthProcessor extends AudioWorkletProcessor {
   noteOn: (channel: number, key: number, vel: number, delayTime: number) => void
